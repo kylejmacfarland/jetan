eef1b79adccd65c95b5abd18649586736825de7f
Kyle Mac <kylejmacfarland@gmail.com>
2024-06-14 10:23:08 -0400

    window handling (rendering, quit requests) and AI logic on separate threads

--- src/jetan.c
+++ src/jetan.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include <time.h>
+#include <pthread.h>
 
 #include "logic.h"
 #include "visual.h"
 #include "array.h"
 #include "util.h"
 
+const int FPS = 10;
+
 Board* current;
 
-void playGame() {
-	renderBoard(current);
-	while (!isQuitRequested()) {
-		makeMove(current);
-		renderBoard(current);
-		if (checkLoss(current)) {
-			if (current->orangeToMove) {
+void playGame(Board* board) {
+	while (true) {
+		makeMove(board);
+		if (checkLoss(board)) {
+			if (board->orangeToMove) {
 				printf("Black wins!\n");
 			} else {
 				printf("Orange wins!\n");
 			}
 			return;
+		}
+	}
+}
+
+void handleWindow() {
+	bool running = true;
+	int startTicks, ticksElapsed;
+	while (true) {
+		startTicks = SDL_GetTicks();
+		renderBoard(current);
+		if (isQuitRequested()) {
+			return;
+		}
+		ticksElapsed = SDL_GetTicks() - startTicks;
+		if (ticksElapsed < 1000 / FPS) {
+			SDL_Delay(1000 / FPS - ticksElapsed);
 		}
 	}
 }
@@ -31,7 +48,9 @@ int main() {
 	setRandomSeed(time(NULL));
 	current = malloc(sizeof(Board));
 	setupBoard(current);
-	playGame();
+	pthread_t logicThread;
+	pthread_create(&logicThread, NULL, playGame, current);
+	handleWindow();
 	free(current);
 	destroyVisual();
 	return 0;
